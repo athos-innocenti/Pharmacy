f66147c0d61cd94fd96463b6e48f1e9358d9d997
Athos <prova@test.com>
2020-05-04 18:52:29 +0200

    Singleton fixed

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,41 @@ public class Main {
 
     public static void main(String[] args) throws NoEmployeeException, FullWarehouseException {
 
-        Pharmacy pharmacy = Pharmacy.getIstance();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nome della farmacia: ");
+        String pharmacyName = scanner.nextLine();
+
+        System.out.println("Quanti sono i dipendenti della farmacia? ");
+        int numberOfEmployees = scanner.nextInt();
+        Pharmacy.setNumberOfEmployees(numberOfEmployees);
+        if (numberOfEmployees == 0) {
+            throw new NoEmployeeException();
+        }
+        scanner.nextLine();
+
+        Pharmacist[] pharmacists = new Pharmacist[numberOfEmployees];
+
+        String pharmacistsName;
+        String pharmacistsSurname;
+        System.out.println("Nome del direttore: ");
+        pharmacistsName = scanner.nextLine();
+        System.out.println("Cognome del direttore: ");
+        pharmacistsSurname = scanner.nextLine();
+        pharmacists[0] = new Pharmacist(pharmacistsName, pharmacistsSurname, true);
+
+        for (int i = 1; i < pharmacists.length; i++) {
+            System.out.println("Nome del " + i + "^o dipendente: ");
+            pharmacistsName = scanner.nextLine();
+            System.out.println("Cognome del " + i + "^o dipendente: ");
+            pharmacistsSurname = scanner.nextLine();
+            pharmacists[i] = new Pharmacist(pharmacistsName, pharmacistsSurname, false);
+        }
+
+        Pharmacy pharmacy = Pharmacy.getIstance(numberOfEmployees, pharmacyName, pharmacists);
         pharmacy.listOfPharmacists();
 
         boolean nextClient;
-        Scanner scanner = new Scanner(System.in);
 
         do {
             System.out.println("\nNome del cliente: ");
